f79780d27994cfb9d762afa875dc6989fa9972ad
Paras Upadhyay <paras13upadhyay@gmail.com>
2025-04-05 00:08:32 +0530

    Correct ToolboxProvider import

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,7 +2,7 @@ import Board from "./components/Board";
 import ToolBar from "./components/Toolbar";
 import Toolbox from "./components/Toolbox";
 import BoardProvider from "./store/BoardProvider";
-import ToolboxProvider from "./store/toolboxProvider";
+import ToolboxProvider from "./store/ToolboxProvider";
 
 function App() {
   return (
