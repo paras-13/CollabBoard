e966cdabe05aadad9af42f35fc4732384ebecb2d
Paras Upadhyay <paras13upadhyay@gmail.com>
2025-07-04 20:25:05 +0530

    Deployment correction

--- Frontend/src/components/Room/index.jsx
+++ Frontend/src/components/Room/index.jsx
@@ -1,34 +1,6 @@
-// import React from "react";
-// import classes from "./index.module.css";
-// import { useState } from "react";
-// import ShareRoom from "./shareRoom";
-// const Room = () => {
-//   const [openShareRoomScreen, setOpenShareRoomScreen] = useState(false);
-//   const handleShareRoomClick = () => {
-//     console.log("share room button clicked");
-//     setOpenShareRoomScreen(true);
-//   };
-//   return (
-//     <>
-//       <div className={classes.container}>
-//         <button className={classes.button} onClick={handleShareRoomClick}>
-//           Share Room
-//         </button>
-//         <button className={classes.button}>Join Room</button>
-//       </div>
-//       {openShareRoomScreen && (
-//         <div className={classes.shareRoomScreen}>
-//           <ShareRoom />
-//         </div>
-//       )}
-//     </>
-//   );
-// };
-// export default Room;
-
 import React, { useState } from "react";
 import classes from "./index.module.css";
-import ShareRoom from "./shareRoom";
+import ShareRoom from "./ShareRoom";
 
 const Room = () => {
   const [openShareRoomScreen, setOpenShareRoomScreen] = useState(false);
